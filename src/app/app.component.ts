9ad96a96303c328bf861b0d8b1c64c696cc4e6bb
BenjaminOrtega <eduardobenjamin.cortes@outlook.com>
2021-05-16 19:37:08 -0500

    se agregaron los servicios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit} from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { EstadosService } from "./services/estados/estados.service";
+import { PaisesService } from "./services/paises/paises.service";
+import { PersonasService } from "./services/personas/personas.service";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'GestionPersonasFrontEnd';
+export class AppComponent implements OnInit{
+
+  personaForm!: FormGroup;
+
+  constructor(
+    public fb: FormBuilder,
+    public estadosService: EstadosService,
+    public paisService: PaisesService,
+    public personasService: PaisesService
+  ){
+
+  }
+
+  ngOnInit(): void {
+
+    this.personaForm = this.fb.group({
+      nombre: ['', Validators.required],
+      apellido: ['', Validators.required],
+      edad: ['', Validators.required],
+      pais: ['', Validators.required],
+      estado: ['', Validators.required]
+    });
+
+  }
 }
